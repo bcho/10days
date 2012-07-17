64a9d062d8237ebaaaaf621059a76ce362306e2c
hbc <bcxxxxxx@gmail.com>
2012-07-17 11:13:07 +0800

    finished #intro

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,6 +1,7 @@
 (function() {
     $(document).ready(function() {     
         $('#main_content').xrhyme({
+            animationTime: 800,
             navigationSelector: 'a.nav',
             anchorMode: true,
             easing: 'easeInOutExpo'
